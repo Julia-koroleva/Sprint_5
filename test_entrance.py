bad099fb78416ba14b00016f52b0caac5a743a1c
Julia-koroleva <julia-koroleva@list.ru>
2025-08-30 12:41:38 +0300

    Test entrance second

--- test_entrance.py
+++ test_entrance.py
@@ -6,23 +6,42 @@ from selenium.webdriver.support.wait import WebDriverWait
 from curl import *
 from data import Credantial
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 
-def test_entrance_on_the_main(login_page):
+def test_entrance_on_the_main_first(login_page):
 
     login_page.find_element(*Locators.email_login).send_keys(Credantial.email)
     login_page.find_element(*Locators.password_login).send_keys(Credantial.password)
-    email_field = login_page.find_element(*Locators.email_login)
-    assert email_field.get_attribute("value") == Credantial.email
-    password_field = login_page.find_element(*Locators.password_login)
-    assert password_field.get_attribute("value") == Credantial.password
     login_page.find_element(*Locators.button).click()
     cabinet_element = WebDriverWait(login_page, 5).until(
         expected_conditions.visibility_of_element_located(Locators.cabinet)
     )
     assert cabinet_element.is_displayed()
    
+def test_entrance_on_the_main_second(main_page_start):
+    main_page_start.find_element(*Locators.entrance_button).click()
+    WebDriverWait(main_page_start, 10).until(
+        EC.url_contains("/login")
+    )
+    main_page_start.find_element(*Locators.email_login).send_keys(Credantial.email)
+    main_page_start.find_element(*Locators.password_login).send_keys(Credantial.password)
+    main_page_start.find_element(*Locators.button).click()
+    cabinet_element = WebDriverWait(main_page_start, 5).until(
+        expected_conditions.visibility_of_element_located(Locators.cabinet)
+    )
+    assert cabinet_element.is_displayed()
 
-
-
+def test_entrance_on_the_main_second(main_page_start):
+    main_page_start.find_element(*Locators.entrance_button).click()
+    WebDriverWait(main_page_start, 10).until(
+        EC.url_contains("/login")
+    )
+    main_page_start.find_element(*Locators.email_login).send_keys(Credantial.email)
+    main_page_start.find_element(*Locators.password_login).send_keys(Credantial.password)
+    main_page_start.find_element(*Locators.button).click()
+    cabinet_element = WebDriverWait(main_page_start, 5).until(
+        expected_conditions.visibility_of_element_located(Locators.cabinet)
+    )
+    assert cabinet_element.is_displayed()
 
 
