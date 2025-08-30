8851924b83e6a5a19d92321385e60c5f71f8e717
Julia-koroleva <julia-koroleva@list.ru>
2025-08-30 14:08:56 +0300

    Test entrance 4 metods and test quit

--- test_entrance.py
+++ test_entrance.py
@@ -8,17 +8,7 @@ from data import Credantial
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_entrance_on_the_main_first(login_page):
-
-    login_page.find_element(*Locators.email_login).send_keys(Credantial.email)
-    login_page.find_element(*Locators.password_login).send_keys(Credantial.password)
-    login_page.find_element(*Locators.button).click()
-    cabinet_element = WebDriverWait(login_page, 5).until(
-        expected_conditions.visibility_of_element_located(Locators.cabinet)
-    )
-    assert cabinet_element.is_displayed()
-   
-def test_entrance_on_the_main_second(main_page_start):
+def test_entrance_on_the_main(main_page_start):
 
     main_page_start.find_element(*Locators.entrance_button).click()
     WebDriverWait(main_page_start, 10).until(
@@ -28,7 +18,7 @@ def test_entrance_on_the_main_second(main_page_start):
     main_page_start.find_element(*Locators.password_login).send_keys(Credantial.password)
     main_page_start.find_element(*Locators.button).click()
     cabinet_element = WebDriverWait(main_page_start, 5).until(
-        expected_conditions.visibility_of_element_located(Locators.cabinet)
+        expected_conditions.visibility_of_element_located(Locators.make_order)
     )
     assert cabinet_element.is_displayed()
 
@@ -42,7 +32,7 @@ def test_entrance_on_the_main_cabinet(main_page_start):
     main_page_start.find_element(*Locators.password_login).send_keys(Credantial.password)
     main_page_start.find_element(*Locators.button).click()
     cabinet_element = WebDriverWait(main_page_start, 5).until(
-        expected_conditions.visibility_of_element_located(Locators.cabinet)
+        expected_conditions.visibility_of_element_located(Locators.make_order)
     )
     assert cabinet_element.is_displayed()
 
@@ -64,11 +54,12 @@ def test_entrance_on_registerbuttonclick(main_page_start):
     main_page_start.find_element(*Locators.password_login).send_keys(Credantial.password)
     main_page_start.find_element(*Locators.button).click()
     cabinet_element = WebDriverWait(main_page_start, 5).until(
-        expected_conditions.visibility_of_element_located(Locators.cabinet)
+        expected_conditions.visibility_of_element_located(Locators.make_order)
     )
     assert cabinet_element.is_displayed()
 
 def test_entrance_on_new_password_button(main_page_start):
+
     main_page_start.find_element(*Locators.cabinet).click()
     WebDriverWait(main_page_start, 10).until(
         EC.url_contains("/login")
@@ -85,6 +76,30 @@ def test_entrance_on_new_password_button(main_page_start):
     main_page_start.find_element(*Locators.password_login).send_keys(Credantial.password)
     main_page_start.find_element(*Locators.button).click()
     cabinet_element = WebDriverWait(main_page_start, 5).until(
-        expected_conditions.visibility_of_element_located(Locators.cabinet)
+        expected_conditions.visibility_of_element_located(Locators.make_order)
     )
     assert cabinet_element.is_displayed()
+
+def test_entrance_quit(main_page_start):
+
+    main_page_start.find_element(*Locators.entrance_button).click()
+    WebDriverWait(main_page_start, 10).until(
+        EC.url_contains("/login")
+    )
+    main_page_start.find_element(*Locators.email_login).send_keys(Credantial.email)
+    main_page_start.find_element(*Locators.password_login).send_keys(Credantial.password)
+    main_page_start.find_element(*Locators.button).click()
+    WebDriverWait(main_page_start, 5).until(
+        expected_conditions.visibility_of_element_located(Locators.make_order)
+    )
+    main_page_start.find_element(*Locators.cabinet).click()
+    WebDriverWait(main_page_start, 10).until(
+        EC.url_contains("/account/profile")
+    ) 
+    main_page_start.find_element(*Locators.quit).click()
+    login_element = WebDriverWait(main_page_start, 5).until(
+        expected_conditions.visibility_of_element_located(Locators.button)
+    )
+    assert login_element.is_displayed()
+
+    
